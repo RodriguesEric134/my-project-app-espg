14c2f97e5ce287b0673fdec7c5455a8a7a262d40
RodriguesEric134 <rm550249@fiap.com.br>
2023-08-22 11:57:08 -0300

    Fim da Aula 22/8

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from "react"
+import Cabecalho from "../components/Cabecalho"
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <Cabecalho/>
+        <section>
+          <p><a href="/">Home Page</a></p>
+          <button onClick={()=> alert("Hello Word!")}>
+            Click Me!
+          </button>
+        </section>
+        <footer>
+          <p>&copy; 2021, Vite.js and React</p>
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 100)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  ) 
 }
-
-export default App
